3dde52b2296d59fa4a94b0ca2e8bb220c18a5306
Robert Skrzypczak <robert.skrzypczak21@gmail.com>
2026-09-15 19:48:41 +0200

    add test requirement impl

---
+++ MarketRadar/MarketRadar.Domain/Test.cs
@@ -0,0 +1,22 @@
+var requirement = new Requirement
+{
+    Name = "DailyChange",
+    Instrument = "ASB",
+    ObservationDate = new DateTime(2026, 4, 23),
+    Requirements = [
+        new Requirement
+        {   
+            Name = "DailyChange",
+            Instrument = "ASB",
+            ObservationDate = new DateTime(2026, 4, 23),
+            Requirements = []
+        },
+        new Requirement
+        {   
+            Name = "DailyChange",
+            Instrument = "ASB",
+            ObservationDate = new DateTime(2026, 4, 23),
+            Requirements = []
+        }
+    ]
+};
