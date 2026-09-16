95316c8e564cfb3534f8e7dea8baf15cc0dbf3cf
Robert Skrzypczak <robert.skrzypczak21@gmail.com>
2026-09-16 20:58:14 +0200

    add more test requirements

--- MarketRadar/MarketRadar.Domain/Test.cs
+++ MarketRadar/MarketRadar.Domain/Test.cs
@@ -1,4 +1,12 @@
-var requirement = new Requirement
+var price = new Requirement
+{   
+    Name = "Price",
+    Instrument = "ASB",
+    ObservationDate = new DateTime(2026, 4, 23),
+    Requirements = []
+};
+
+var dailyChange = new Requirement
 {
     Name = "DailyChange",
     Instrument = "ASB",
@@ -15,6 +23,43 @@ var requirement = new Requirement
         {   
             Name = "Price",
             Instrument = "ASB",
+            ObservationDate = new DateTime(2026, 4, 22),
+            Requirements = []
+        }
+    ]
+};
+
+var relativeMomentum12M = new Requirement
+{
+    Name = "RelativeMomentum12M",
+    Instrument =  "ASB",
+    ObservationDate = new DateTime(2026, 4, 23),
+    Requirements = [
+        new Requirement
+        {   
+            Name = "Price",
+            Instrument = "ASB",
+            ObservationDate = new DateTime(2026, 4, 23),
+            Requirements = []
+        },
+        new Requirement
+        {   
+            Name = "Price",
+            Instrument = "ASB",
+            ObservationDate = new DateTime(2026, 4, 22),
+            Requirements = []
+        },
+        new Requirement
+        {   
+            Name = "Price",
+            Instrument = "WIG",
+            ObservationDate = new DateTime(2026, 4, 23),
+            Requirements = []
+        },
+        new Requirement
+        {   
+            Name = "Price",
+            Instrument = "WIG",
             ObservationDate = new DateTime(2026, 4, 22),
             Requirements = []
         }
