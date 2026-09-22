81908613cf18454ae63b9aed5210490fc784b6f4
Robert Skrzypczak <robert.skrzypczak21@gmail.com>
2026-09-22 22:47:47 +0200

    add data requests

--- MarketRadar/MarketRadar.Domain/Requirements/Base/NetIncome.cs
+++ MarketRadar/MarketRadar.Domain/Requirements/Base/NetIncome.cs
@@ -3,3 +3,7 @@ public class NetIncome : IRequirement
     public required string Instrument { get; init; }
     public required DateTime ObservationDate { get; init; }
 }
+
+public record NetIncomeRequest(string Instrument, DateTime At);
+
+public record NetIncomeResponse(decimal Value);
--- MarketRadar/MarketRadar.Domain/Requirements/Base/Price.cs
+++ MarketRadar/MarketRadar.Domain/Requirements/Base/Price.cs
@@ -3,3 +3,7 @@ public class Price : IRequirement
     public required string Instrument { get; init; }
     public required DateTime ObservationDate { get; init; }
 }
+
+public record PriceRequest(string Instrument, DateTime At);
+
+public record PriceResponse(decimal Value);
